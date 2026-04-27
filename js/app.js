77e5086f955b9ae4c2c1d83b2b9ebdcfa3329888
Martin-Varela4 <martinchovarela2@gmail.com>
2026-04-27 13:49:41 -0300

    Funciones básicas terminadas para los ingresos/gastos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const formulario = document.querySelector('#form-gasto')
-const listaGasos = document.querySelector('#lista-gastos')
+const listaGastos = document.querySelector('#lista-gastos')
 const balanceTotal = document.querySelector('#balance')
 
 const descripcionInput = document.querySelector('#descripcion')
@@ -25,3 +25,43 @@ formulario.addEventListener('submit', function(e) {
 
 
 });
+
+//array para almacenar los gastos
+const gastos = []
+//funcion para agregar un gasto a la lista
+function agregarGasto(desc, valor, cat) {
+
+    const montoNumero = Number(valor);
+    //agregar el gasto al array de gastos
+    gastos.push(montoNumero);
+
+
+    //crear un elemento de lista (li)
+    const nuevoGasto = document.createElement('li');
+    
+    
+    nuevoGasto.innerHTML = `
+        <strong>${desc}</strong> - $${montoNumero} 
+        <small>(${cat})</small>
+    `;
+
+    
+    listaGastos.appendChild(nuevoGasto);
+
+    //actualizar el balance total
+    actualizarBalance();
+    
+}
+
+
+function actualizarBalance() {
+    let = total = 0;
+    
+    for (let i = 0; i < gastos.length; i++) {
+        total += gastos[i];
+    }
+    //Mostrar el balance total
+    balanceTotal.textContent= `$${total}`;
+}
+
+
